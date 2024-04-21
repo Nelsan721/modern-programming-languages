614b99fd711522b08c24f0eb9dda0ed80b3dd9e3
Nelsan721 <128516199+Nelsan721@users.noreply.github.com>
2024-04-21 19:46:25 +0500

    Laba4

---
+++ Lab4/src/main/java/com/example/lab4/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.lab4.service;
+
+import com.example.lab4.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface StudentService {
+
+    List<Student> getAllStudents();
+
+    Student getStudent(int id);
+
+    Student saveStudent(Student student);
+
+    void deleteStudent(int id);
+}
