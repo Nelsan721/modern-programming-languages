0e3534f165ff6ea9e5b0667e45055f2bf7215f63
Nelsan721 <128516199+Nelsan721@users.noreply.github.com>
2024-04-21 19:36:21 +0500

    Laba1

---
+++ Lab1/src/main/java/com/example/lab1/controller/MyController.java
@@ -0,0 +1,32 @@
+package com.example.lab1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.lab1.model.Request;
+import com.example.lab1.model.Response;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class MyController {
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request){
+
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
